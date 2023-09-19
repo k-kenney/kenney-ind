9e105fc1a36b561f5f9c953c33c3187a71b38021
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-09-19 18:12:00 +0300

    updated triangle

--- src/pages/About/components/Header.jsx
+++ src/pages/About/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Fade>
       <div className="w-full">
-        <div className="relative bg-right float-right bg-no-repeat m-auto w-3/4 md:w-1/2">
+        <div className="relative bg-right float-right bg-no-repeat m-auto w-1/2 md:w-1/3">
           <img src={triangle} alt="red triangle" className="w-screen h-screen" />
         </div>
         <div className="absolute m-auto top-0 bottom-0 right-0 left-0 z-50">
