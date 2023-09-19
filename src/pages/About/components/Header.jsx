3c19fd89bed8a7c0d054c20b54a200b6e729eeef
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-09-19 18:06:35 +0300

    fixed triangle

--- src/pages/About/components/Header.jsx
+++ src/pages/About/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <Fade>
       <div className="w-full">
-        <div className="relative bg-right float-right bg-no-repeat m-auto aspect-square w-3/5">
-          <img src={triangle} alt="red triangle" className="w-full h-full" />
+        <div className="relative bg-right float-right bg-no-repeat m-auto w-3/4 md:w-1/2">
+          <img src={triangle} alt="red triangle" className="w-screen h-screen" />
         </div>
         <div className="absolute m-auto top-0 bottom-0 right-0 left-0 z-50">
           <Navbar />
