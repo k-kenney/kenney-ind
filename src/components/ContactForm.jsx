2ec2d2ea45b3c5203ba055ce3cd423b68c215750
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-09-23 11:23:26 +0300

    trying

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,51 +1,28 @@
 import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
   // const [isSubmitted, setIsSubmitted] = useState(false);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const handleSubmit = event => {
-  //   event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
   
-  //   // 👇️ redirect
-  //   navigate('/', {replace: true});
-  // };
-
-  const [formData, setFormData] = useState({ name: '', message: '' });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData).toString(),
-      });
-
-      // After successful submission, redirect to your homepage
-      window.location.href = '/';
-    } catch (error) {
-      console.error('Form submission error:', error);
-    }
+    // 👇️ redirect
+    navigate('/', {replace: true});
   };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  
 
   return (
     <div>
         <form
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           name="contact"
           method="POST"
           data-netlify-honeypot="bot-field"
           action="/"
-          onSubmit={handleSubmit}
         >
           <input type="hidden" name="form-name" value="contact" />
 
@@ -60,9 +37,8 @@ const ContactForm = () => {
               id="name"
               name="name"
               placeholder="Your Name"
-              value={formData.name} 
-              onChange={handleChange}
-              // onChange={(e) => setName(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div>
@@ -71,9 +47,8 @@ const ContactForm = () => {
               name="message"
               id="message"
               placeholder="Your Message"
-              value={formData.message} 
-              onChange={handleChange}
-              // onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
           <button type="submit">Send Message</button>
