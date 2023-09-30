36c6997e9827d218d33e79f0e12f4da4da661448
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-09-30 09:58:21 +0300

    edited contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
-import gears from "../assets/img/contactWheels.png";
 
 const ContactForm = () => {
   const [company, setCompany] = useState("");
@@ -11,6 +10,26 @@ const ContactForm = () => {
   const [rows, setRows] = useState(10); // Default number of rows
 
   const navigate = useNavigate();
+
+   // Function to format the phone number
+   const formatPhoneNumber = (input) => {
+    // Remove all non-digit characters
+    const phoneNumber = input.replace(/\D/g, "");
+
+    // Format the phone number as (xxx) xxx-xxxx
+    const formattedPhoneNumber = phoneNumber.replace(
+      /(\d{3})(\d{3})(\d{4})/,
+      "($1) $2-$3"
+    );
+
+    return formattedPhoneNumber;
+  };
+
+  const handleNumberChange = (e) => {
+    // Ensure only digits and a few specific characters are allowed in the input
+    const formattedValue = formatPhoneNumber(e.target.value);
+    setNumber(formattedValue);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -63,128 +82,132 @@ const ContactForm = () => {
 
   return (
     <>
-        <div className="contact-image">
-          <div className="">
-            <h1 className="text-center text-3xl xl:text-4xl m-0 pt-14 xl:pt-16">
-              Contact Us
-            </h1>
-            <form
-              onSubmit={handleSubmit}
-              name="contact"
-              method="POST"
-              data-netlify-honeypot="bot-field"
-              action="/success"
-              className="w-full h-screen flex flex-col items-center justify-center md:flex-row md:items-start mx-auto my-auto"
-            >
-              <input type="hidden" name="form-name" value="contact" />
+      <div className="contact-image">
+        <div className="">
+          <h1 className="text-center text-3xl xl:text-4xl m-0 pt-14 xl:pt-16">
+            Contact Us
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            name="contact"
+            method="POST"
+            data-netlify-honeypot="bot-field"
+            action="/success"
+            className="w-full h-screen flex flex-col items-center justify-center md:flex-row md:items-start mx-auto my-auto"
+          >
+            <input type="hidden" name="form-name" value="contact" />
 
-              <div hidden>
-                <input name="bot-field" />
-              </div>
-              <div className="md:flex md:justify-center md:m-0 md:items-center md:gap-20 md:pt-8 lg:gap-32 xl:gap-40 xl:pt-12">
+            <div hidden>
+              <input name="bot-field" />
+            </div>
+            <div className="md:flex md:justify-center md:m-0 md:items-center md:gap-20 md:pt-8 lg:gap-32 xl:gap-40 xl:pt-12">
+              <div className="">
                 <div className="">
+                  <div className="contact-text">
+                    <label className="" htmlFor="company">
+                      Company*
+                    </label>
+                  </div>
                   <div className="">
-                    <div className="contact-text">
-                      <label className="" htmlFor="company">
-                        Company
-                      </label>
-                    </div>
-                    <div className="">
-                      <input
-                        className="contact-input"
-                        type="text"
-                        id="company"
-                        name="company"
-                        placeholder="Company Name"
-                        value={company}
-                        onChange={(e) => setCompany(e.target.value)}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className="contact-text">
-                      <label htmlFor="name">Full Name</label>
-                    </div>
-                    <div>
-                      <input
-                        className="contact-input"
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Your Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className="contact-text">
-                      <label htmlFor="number">Phone Number</label>
-                    </div>
-                    <div>
-                      <input
-                        className="contact-input"
-                        type="tel"
-                        id="number"
-                        name="number"
-                        placeholder="Your Number"
-                        value={number}
-                        onChange={(e) => setNumber(e.target.value)}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className="contact-text">
-                      <label htmlFor="email">Email</label>
-                    </div>
-                    <div>
-                      <input
-                        className="contact-input"
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </div>
+                    <input
+                      className="contact-input"
+                      type="text"
+                      id="company"
+                      name="company"
+                      placeholder="Company Name"
+                      value={company}
+                      onChange={(e) => setCompany(e.target.value)}
+                      required
+                    />
                   </div>
                 </div>
-                <div className="">
-                  <div>
-                    <div className="contact-text">
-                      <label htmlFor="message">Message</label>
-                    </div>
-                    <div>
-                      <textarea
-                        className="contact-input "
-                        name="message"
-                        id="message"
-                        placeholder="Your Message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        rows={rows}
-                      ></textarea>
-                    </div>
-                  </div>
 
-                  <div className="">
-                    <button
-                      type="submit"
-                      className="bg-copyright bg-opacity-40 border rounded-lg py-2 px-3 text-white text-lg font-special my-5 lg:py-3 lg:px-5 lg:text-xl transition-all duration-500 hover:bg-gray-600"
-                    >
-                      Submit
-                    </button>
+                <div>
+                  <div className="contact-text">
+                    <label htmlFor="name">Full Name*</label>
+                  </div>
+                  <div>
+                    <input
+                      className="contact-input"
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="Your Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="contact-text">
+                    <label htmlFor="number">Phone Number*</label>
+                  </div>
+                  <div>
+                    <input
+                      className="contact-input"
+                      type="tel"
+                      id="number"
+                      name="number"
+                      placeholder="(xxx)xxx-xxxx"
+                      value={number}
+                      onChange={handleNumberChange}
+                      required // Make the field required
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="contact-text">
+                    <label htmlFor="email">Email*</label>
+                  </div>
+                  <div>
+                    <input
+                      className="contact-input"
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
                   </div>
                 </div>
               </div>
-            </form>
-          </div>
+              <div className="">
+                <div>
+                  <div className="contact-text">
+                    <label htmlFor="message">Message*</label>
+                  </div>
+                  <div>
+                    <textarea
+                      className="contact-input "
+                      name="message"
+                      id="message"
+                      placeholder="Your Message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      rows={rows}
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+
+                <div className="">
+                  <button
+                    type="submit"
+                    className="bg-copyright bg-opacity-40 border rounded-lg py-2 px-3 text-white text-lg font-special my-5 lg:py-3 lg:px-5 lg:text-xl transition-all duration-500 hover:bg-gray-600"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
-
+      </div>
     </>
   );
 };
