a6b6334f631d91fdbdebd0297f926726d3c03cba
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-09-30 10:18:14 +0300

    commit

--- src/pages/Careers/components/CareersHeader.jsx
+++ src/pages/Careers/components/CareersHeader.jsx
@@ -4,7 +4,7 @@ const CareersHeader = () => {
       <div className="absolute top-52 bottom-0 left-0 right-36 sm:right-64 md:right-0 z-20">
         <h1 className="triangle-header-text">Careers</h1>
       </div>
-      <div className="absolute top-80 sm:top-96 bottom-0 left-0 right-0 z-20 mt-14 lg:mt-20 sm:my-2 space-y-12 w-1/4 md:text-2xl m-auto text-center md:leading-12 text-2xl leading-10">
+      <div className="absolute top-80 sm:top-96 bottom-0 left-0 right-0 z-20 mt-14 lg:mt-20 sm:my-2 space-y-12 md:w-1/4 w-2/5 md:text-2xl m-auto text-center md:leading-12 text-2xl leading-10">
         <p>Ready to take the next step in your career?</p>
       </div>
     </div>
