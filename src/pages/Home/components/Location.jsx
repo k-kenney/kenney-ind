4876ec0ae0a6762e36d15d67e3ef7e6cd09d5a34
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-10-01 12:42:10 +0300

    fixed spacing

--- src/pages/Home/components/Location.jsx
+++ src/pages/Home/components/Location.jsx
@@ -22,7 +22,7 @@ function GoogleMap() {
 
   return (
     <>
-        <div className='pt-12 pb-32 md:flex md:gap-20 md:w-4/5 lg:w-2/3 md:items-center m-auto'>
+        <div className='py-32 md:flex md:gap-20 md:w-4/5 lg:w-2/3 md:items-center m-auto'>
             <div className='flex flex-col justify-center items-center pb-14'>
                 <h1 className='text-3xl xl:text-4xl pb-6'>Location</h1>
                 <a href={googleMapsLink} target="_blank" rel="noopener noreferrer">
