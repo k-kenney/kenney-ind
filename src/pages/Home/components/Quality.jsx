e4ff00620a1a050e9fc5268cd75ec7d68d542c9b
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-10-26 07:48:15 +0300

    added quality section to homepage

--- src/pages/Home/components/Quality.jsx
+++ src/pages/Home/components/Quality.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
 
 const Quality = () => {
   return (
-    <div>
-        <h1 className='text-2xl text-center'>Quality</h1>
+    <div className="m-auto flex items-center justify-center flex-col w-2/3 pt-14 pb-12 lg:flex-row">
+      <h1 className="text-3xl xl:text-4xl p-9 text-center uppercase">
+        Quality
+      </h1>
+      <p className="text-2xl text-center xl:text-3xl w-5/6 py-5 font-special lg:border-l lg:pl-14">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque,
+        accusantium reiciendis beatae obcaecati doloribus nisi quod
+        reprehenderit pariatur enim dolor distinctio. Corporis ex accusantium
+        repellendus beatae deserunt delectus commodi?
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Quality
+export default Quality;
